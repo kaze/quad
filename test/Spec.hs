dd5317412d68fcec1488b7421c66d07235d07097
zsolt kormany <zsoltkormany@gmail.com>
2021-05-30 07:15:25 +0200

    add test for Docker.Service

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,15 @@
 module Main where
 
+import Core
 import qualified Docker
 import RIO
-import Core
+import qualified RIO.Map as Map
 import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import Test.Hspec
+
 
 -- Helper funcctions
+
 makeStep :: Text -> Text -> [Text] -> Step
 makeStep name image commands
   = Step
@@ -18,7 +22,9 @@ makePipeline :: [Step] -> Pipeline
 makePipeline steps =
   Pipeline { steps = NonEmpty.Partial.fromList steps }
 
+
 -- Test values
+
 testPipeline :: Pipeline
 testPipeline = makePipeline
   [ makeStep "First step" "ubuntu" ["date"]
@@ -32,5 +38,30 @@ testBuild = Build
   , completedSteps = mempty
   }
 
+runBuild :: Docker.Service -> Build -> IO Build
+runBuild docker build = do
+  newBuild <- Core.progress docker build
+
+  case newBuild.state of
+    BuildFinished _ ->
+      pure newBuild
+    _ -> do
+      threadDelay (1 * 1000 * 1000)
+      runBuild docker newBuild
+
+
+-- First test
+
+testRunSuccess :: Docker.Service -> IO ()
+testRunSuccess docker = do
+  result <- runBuild docker testBuild
+  result.state `shouldBe` BuildFinished BuildSucceeded
+  Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
+
 main :: IO ()
-main = pure ()
+main = hspec do
+  docker <- runIO Docker.createService
+
+  describe "Quad CI" do
+    it "should run a build (success)" do
+      testRunSuccess docker
